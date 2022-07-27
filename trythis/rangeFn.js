7fd8732ad0155378a415d180056b3b787e4ad1fa
ho2yahh <ho2yahh@gmail.com>
2022-07-27 20:07:49 +0900

    range 함수 구현

--- trythis/rangeFn.js
+++ trythis/rangeFn.js
@@ -7,9 +7,13 @@ const range = (firstN, endN, r) => {
         for (; i <= endN; i += range) {
             res.push(i);
         }
-    } else {
+    } else if (firstN > endN) {
         range = r ?? -1;
         for (; i >= endN; i += range) {
+            res.push(i);
+        }
+    } else if (!endN) {
+        for (let i = 1; i <= firstN; i += 1) {
             res.push(i);
         }
     }
@@ -22,6 +26,6 @@ range(1, 10); // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
 range(10, 1); // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 range(10, 1, -1); // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 range(10, 1, -2); // [ 10, 8, 6, 4, 2 ]
-return;
 range(5); // [1, 2, 3, 4, 5]
 range(100); // [1, 2, 3, 4, 5, …, 99, 100]
+return;

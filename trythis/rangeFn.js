57f66d0023a8adbf86685353fd40c0f4ab1fdc9a
ho2yahh <ho2yahh@gmail.com>
2022-07-27 19:57:31 +0900

    trythis rangeFn 6개 테스트 케이스 구현

--- trythis/rangeFn.js
+++ trythis/rangeFn.js
@@ -1,18 +1,27 @@
 const range = (firstN, endN, r) => {
     let i = firstN;
+    let range = r ?? 1;
     let res = [];
-    for (; i <= endN; i += r) {
-        res.push(i);
+
+    if (firstN < endN) {
+        for (; i <= endN; i += range) {
+            res.push(i);
+        }
+    } else {
+        range = r ?? -1;
+        for (; i >= endN; i += range) {
+            res.push(i);
+        }
     }
     return res, console.log(res);
 };
 
 range(1, 10, 1); // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
 range(1, 10, 2); // [1, 3, 5, 7, 9]
-return;
 range(1, 10); // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
 range(10, 1); // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 range(10, 1, -1); // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 range(10, 1, -2); // [ 10, 8, 6, 4, 2 ]
+return;
 range(5); // [1, 2, 3, 4, 5]
 range(100); // [1, 2, 3, 4, 5, …, 99, 100]

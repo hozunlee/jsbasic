1280895557f7b674a62f348c7aad6b9ed67178db
ho2yahh <ho2yahh@gmail.com>
2022-08-01 15:10:40 +0900

    인자 갯수별 range함수

--- trythis/rangeFn3.js
+++ trythis/rangeFn3.js
@@ -1,8 +1,14 @@
 const range = (...args) => {
-    const [start, end, step] = args;
+    let [start, end, step] = args;
 
+    const temp = start;
+    end = end ?? ((start = 1), temp);
+    step = step ?? (start < end ? 1 : -1);
+
+    console.log(start, end, step);
+    console.log(start < end);
     res = [];
-    for (let i = start; i <= end; i += step) {
+    for (let i = start; start < end ? i <= end : i >= end; i += step) {
         res.push(i);
     }
 
@@ -11,12 +17,12 @@ const range = (...args) => {
 
 range(1, 10, 1); // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
 range(1, 10, 2); // [1, 3, 5, 7, 9]
-return;
 range(1, 10); // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
 range(10, 1); // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 range(10, 1, -2); // [10, 8, 6, 4, 2]
 range(5); // [1, 2, 3, 4, 5]
 range(100); // [1, 2, 3, 4, 5, …, 99, 100]
+return;
 range(-5); // [-5, -4, -3, -2, -1]
 range(5, 5); // [5]
 range(5, 5, 0); // [5]

dca03d5262735a273b56beb71e83fd4ebd13409d
ho2yahh <ho2yahh@gmail.com>
2022-08-01 16:24:27 +0900

    feat : 최종 함수구현

--- trythis/rangeFn3.js
+++ trythis/rangeFn3.js
@@ -2,8 +2,14 @@ const range = (...args) => {
     let [start, end, step] = args;
 
     const temp = start;
-    end = end ?? (start < 0 ? 0 : ((start = 1), temp));
+    end = end ?? (start <= 0 ? 0 : ((start = 1), temp));
     step = step ?? (start < end ? 1 : -1);
+
+    if (start === end) {
+        return [start], console.log([start]);
+    } else if ((start < end && step < 0) || (start > end && step > 0)) {
+        return [], console.log([]);
+    }
 
     console.log(start, end, step);
     console.log(start < end);
@@ -15,27 +21,30 @@ const range = (...args) => {
     return res, console.log(res);
 };
 
+range(0); // [0]
+range(1, 5, -1); // []
+range(5, 1, 1); // []
+
 // range(1, 10, 1); // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
 // range(1, 10, 2); // [1, 3, 5, 7, 9]
 // range(1, 10); // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
 // range(10, 1); // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 // range(10, 1, -2); // [10, 8, 6, 4, 2]
+
 // range(5); // [1, 2, 3, 4, 5]
 // range(100); // [1, 2, 3, 4, 5, …, 99, 100]
-range(-5); // [-5, -4, -3, -2, -1]
-return;
-range(5, 5); // [5]
-range(5, 5, 0); // [5]
-range(0, 5); // [0, 1, 2, 3, 4, 5]
-range(5, 5, -1); // [5]
-range(0, -1); // [0, -1]
-range(5, 1, 1); // []
-range(0, -3); // [0, -1, -2, -3]
-range(1, 5, -1); // []
-range(-3, 0); // [-3, -2, -1, 0]
-range(1, 5, 6); // [1]
-range(0); // [0]
-range(0, 0); // [0]
-range(0, 0, 5); // [0]
-range(2, 1, -5); // [2]
-range(0, -1, -5); // [0]
+// range(-5); // [-5, -4, -3, -2, -1]
+
+// range(5, 5); // [5]
+// range(5, 5, 0); // [5]
+// range(5, 5, -1); // [5]
+// range(0, 0, 5); // [0]
+// range(0, 0); // [0]
+
+// range(0, 5); // [0, 1, 2, 3, 4, 5]
+// range(0, -1); // [0, -1]
+// range(0, -3); // [0, -1, -2, -3]
+// range(-3, 0); // [-3, -2, -1, 0]
+// range(1, 5, 6); // [1]
+// range(2, 1, -5); // [2]
+// range(0, -1, -5); // [0]

5c77abe07ff9a4cd5be057be4ece5cf0fb20ae66
ho2yahh <ho2yahh@gmail.com>
2022-08-01 15:22:44 +0900

    feat : start 값이 음수인 1자리 인자

--- trythis/rangeFn3.js
+++ trythis/rangeFn3.js
@@ -2,7 +2,7 @@ const range = (...args) => {
     let [start, end, step] = args;
 
     const temp = start;
-    end = end ?? ((start = 1), temp);
+    end = end ?? (start < 0 ? 0 : ((start = 1), temp));
     step = step ?? (start < end ? 1 : -1);
 
     console.log(start, end, step);
@@ -15,15 +15,15 @@ const range = (...args) => {
     return res, console.log(res);
 };
 
-range(1, 10, 1); // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
-range(1, 10, 2); // [1, 3, 5, 7, 9]
-range(1, 10); // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
-range(10, 1); // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-range(10, 1, -2); // [10, 8, 6, 4, 2]
-range(5); // [1, 2, 3, 4, 5]
-range(100); // [1, 2, 3, 4, 5, …, 99, 100]
-return;
+// range(1, 10, 1); // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
+// range(1, 10, 2); // [1, 3, 5, 7, 9]
+// range(1, 10); // [1, 2, 3, 4,  5, 6, 7, 8, 9, 10]
+// range(10, 1); // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+// range(10, 1, -2); // [10, 8, 6, 4, 2]
+// range(5); // [1, 2, 3, 4, 5]
+// range(100); // [1, 2, 3, 4, 5, …, 99, 100]
 range(-5); // [-5, -4, -3, -2, -1]
+return;
 range(5, 5); // [5]
 range(5, 5, 0); // [5]
 range(0, 5); // [0, 1, 2, 3, 4, 5]

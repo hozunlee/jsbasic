2ca1b0487c52d27960187b89dbc866cc9365ce3e
ho2yahh <ho2yahh@gmail.com>
2022-08-06 11:53:50 +0900

    [tt] 성호님코드 추가

--- 0806/pushpop.js
+++ 0806/pushpop.js
@@ -7,16 +7,19 @@ const push = (arr, ...rest) => {
     return newArr;
 };
 
+// const push = (arr, ...rest) => [...arr, ...rest]; // 성호님코드
+
 console.log(push(arr, 5, 6)); // [1, 2, 3, 4, 5, 6]
 
-const pop = (arr, step) => {
-    const range = step ?? 1;
+const pop = (arr, step = 1) => {
     const newArr = [];
-    for (let i = 0; i < arr.length - range; i += 1) {
+    for (let i = 0; i < arr.length - step; i += 1) {
         newArr.push(arr[i]);
     }
     return newArr;
 };
+//filter를 쓰는 방법도 있음
+// const pop = (arr, cnt = 1) => arr.filter((a, i) => i < arr.length - cnt)
 
 console.log(pop(arr)); // [1, 2, 3, 4, 5] // [1, 2, 3]
 console.log(pop(arr, 2)); // 2개 팝! ⇒ [1, 2, 3] // [1, 2]
@@ -29,15 +32,15 @@ const unshift = (arr, ...rest) => {
 console.log(unshift(arr, 0)); // [0, 1, 2] // [ 0, 1, 2, 3, 4 ]
 console.log(unshift(arr, 7, 8)); // [7, 8, 0, 1, 2] // [ 7, 8, 1, 2, 3, 4 ]
 
-const shift = (arr, step) => {
-    const range = step ?? 1;
-
+const shift = (arr, step = 1) => {
     const newArr = [];
-    for (let i = range; i <= arr.length - 1; i += 1) {
+    for (let i = step; i <= arr.length - 1; i += 1) {
         newArr.push(arr[i]);
     }
     return newArr;
 };
+
+// const shift = (arr, cnt = 1) => arr.filter((a, i) => i < arr.length >= cnt)
 
 console.log(shift(arr)); // [8, 0, 1, 2] // [2,3,4]
 console.log(shift(arr, 2)); // [1, 2] // [3,4]
